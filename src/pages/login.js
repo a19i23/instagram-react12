32fd86e5343019f634364942d587f929364622da
Alan Villa <a19i23@hotmail.com>
2020-09-11 12:53:13 -0700

    Finished week 4 module 2

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,10 +1,101 @@
-import React from "react";
-import { useLoginPageStyles } from "../styles";
+import {
+  Button,
+  Card,
+  CardHeader,
+  TextField,
+  Typography,
+} from '@material-ui/core';
+import React from 'react';
+import { Link } from 'react-router-dom';
+import SEO from '../components/shared/Seo';
+import { useLoginPageStyles } from '../styles';
+import FaceBookIconBlue from '../images/facebook-icon-blue.svg';
+import FaceBookIconWhite from '../images/facebook-icon-white.png';
 
 function LoginPage() {
-  useLoginPageStyles();
+  const classes = useLoginPageStyles();
 
-  return <div>LoginPage</div>;
+  return (
+    <>
+      <SEO title="Login" />
+      <section className={classes.section}>
+        <article>
+          <Card className={classes.card}>
+            <CardHeader className={classes.cardHeader} />
+            <form>
+              <TextField
+                fullWidth
+                variant="filled"
+                label="Username"
+                margin="dense"
+                className={classes.textField}
+                autoComplete="username"
+              />
+              <TextField
+                fullWidth
+                variant="filled"
+                label="Password"
+                type="password"
+                margin="dense"
+                className={classes.textField}
+                autoComplete="current-password"
+              />
+              <Button
+                variant="contained"
+                fullWidth
+                color="primary"
+                className={classes.button}
+                type="submit"
+              >
+                Log In
+              </Button>
+            </form>
+            <div className={classes.orContainer}>
+              <div className={classes.orLine} />
+              <div>
+                <Typography variant="body2" color="textSecondary">
+                  OR
+                </Typography>
+              </div>
+              <div className={classes.orLine} />
+            </div>
+            <LoginWithFacebook color="secondary" iconColor="blue" />
+            <Button fullWidth color="secondary">
+              <Typography variant="caption">Forgot password?</Typography>
+            </Button>
+          </Card>
+
+          <Card className={classes.signUpCard}>
+            <Typography variant="body2" align="right">
+              Don't have an account?
+            </Typography>
+            <Link to="/accounts/emailsignup">
+              <Button color="primary" className={classes.signUpButton}>
+                Sign up
+              </Button>
+            </Link>
+          </Card>
+        </article>
+      </section>
+    </>
+  );
+}
+
+export function LoginWithFacebook({ color, iconColor, variant }) {
+  const classes = useLoginPageStyles();
+  const facebookIcon =
+    iconColor === 'blue' ? FaceBookIconBlue : FaceBookIconWhite;
+
+  return (
+    <Button fullWidth color={color} variant={variant}>
+      <img
+        src={facebookIcon}
+        alt="facebook icon"
+        className={classes.facebookIcon}
+      />
+      Log In with Facebook
+    </Button>
+  );
 }
 
 export default LoginPage;
